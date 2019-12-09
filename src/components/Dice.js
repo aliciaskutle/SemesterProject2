4a03c4b929d52965db332f1bee0cfc5c72ee0b5d
Alicia Skutle <alicia.skutle@gmail.com>
2019-12-09 22:52:31 +0100

    Change Player Position

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -7,8 +7,32 @@ function Dice(props) {
   const handleDiceThrow = () => {
     const diceRoll = Math.floor(Math.random() * 6 + 1);
     setEyes(diceRoll);
-    props.setPlayers();
-    console.log(props.players);
+    // Player 1 = props.players[0]
+    // Player 2 = props.players[1]
+
+    if (props.player === "1") {
+      props.setPlayers(
+        props.selectedPlayers.map((player, index) => {
+          if (index === 0) {
+            return { ...player, position: player.position + diceRoll };
+          } else {
+            return player;
+          }
+        })
+      );
+    }
+
+    if (props.player === "2") {
+      props.setPlayers(
+        props.selectedPlayers.map((player, index) => {
+          if (index === 1) {
+            return { ...player, position: player.position + diceRoll };
+          } else {
+            return player;
+          }
+        })
+      );
+    }
   };
 
   return (
